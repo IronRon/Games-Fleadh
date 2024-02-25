3eebb65d50084e11104ff5da5f5dd3d155676b68
Ronak <ronakfeedback@gmail.com>
2024-02-25 15:23:34 +0000

    Random Procedurally Generate Simple 3D Level

--- Player.gd
+++ Player.gd
@@ -39,6 +39,9 @@ func _physics_process(delta):
 		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 			velocity.y = jump
 			
+		
+		if Input.is_action_just_pressed("Create"):
+			$"../GridMap".set_cell_item($PlayerMesh.global_transform.origin - Vector3(0,2,0), 2)
 
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
